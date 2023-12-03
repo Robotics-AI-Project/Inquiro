c4d4688726873b95a6f45660f7d12b3d6b2845f8
tontan2545 <tontan2545@gmail.com>
2023-12-03 19:38:52 +0700

    feat: visualization options list

---
+++ constants/data.tsx
@@ -0,0 +1,42 @@
+import BarChart from "@/components/pages/chat/visualization/bar-chart";
+import DataTable from "@/components/pages/chat/visualization/data-table";
+import LineGraph from "@/components/pages/chat/visualization/line-graph";
+import { default as visPieChart } from "@/components/pages/chat/visualization/pie-chart";
+import { DataVisualizationProps } from "@/types/data";
+import { BarChartBig, Grid3X3, LineChart, PieChart } from "lucide-react";
+
+export const visualizationList: Readonly<
+  {
+    key: string;
+    name: string;
+    icon: any;
+    visualizationComponent?: React.FC<DataVisualizationProps>;
+  }[]
+> = [
+  {
+    key: "data-table",
+    name: "Data Table",
+    icon: Grid3X3,
+    visualizationComponent: DataTable,
+  },
+  {
+    key: "bar-graph",
+    name: "Bar Graph",
+    icon: BarChartBig,
+    visualizationComponent: BarChart,
+  },
+  {
+    key: "line-graph",
+    name: "Line Graph",
+    icon: LineChart,
+    visualizationComponent: LineGraph,
+  },
+  {
+    key: "pie-chart",
+    name: "Pie Chart",
+    icon: PieChart,
+    visualizationComponent: visPieChart,
+  },
+] as const;
+
+export type VisualizationKey = (typeof visualizationList)[number]["key"];
