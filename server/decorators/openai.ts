d7666944a9e358361723623ff7d0d3d2113f7128
tontan2545 <tontan2545@gmail.com>
2024-01-07 03:42:29 +0700

    feat: openAI decorator

---
+++ server/decorators/openai.ts
@@ -0,0 +1,3 @@
+import OpenAI from "openai";
+
+export const openAIClient = new OpenAI();
--- server/setup.ts
+++ server/setup.ts
@@ -1,6 +1,7 @@
 import swagger from "@elysiajs/swagger";
 import Elysia from "elysia";
 import { swaggerConfig } from "./configs/swagger";
+import { openAIClient } from "./decorators/openai";
 import { deriverSetup } from "./derivers";
 
 export const publicBackend = new Elysia().get("/health", () => "OK", {
@@ -9,7 +10,9 @@ export const publicBackend = new Elysia().get("/health", () => "OK", {
   },
 });
 
-export const basePrivateBackend = new Elysia().derive(deriverSetup);
+export const basePrivateBackend = new Elysia()
+  .decorate("openai", openAIClient)
+  .derive(deriverSetup);
 
 export const intializeBaseBackend = () => {
   let baseBackend = new Elysia({ prefix: "/api" });
