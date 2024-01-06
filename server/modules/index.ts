7831adea84e32da037eb3f48b792b8d495dc9ae9
tontan2545 <tontan2545@gmail.com>
2024-01-07 03:08:03 +0700

    comment: typescript type compilation issue

--- server/modules/index.ts
+++ server/modules/index.ts
@@ -2,6 +2,7 @@ import { basePrivateBackend } from "../setup";
 import { chatModule } from "./chat";
 import { snippetModule } from "./snippet";
 
-export const registerModule = (baseApp: typeof basePrivateBackend) => {
-  return baseApp.use(chatModule).use(snippetModule);
+export const registerModule = (app: typeof basePrivateBackend) => {
+  // Could not iteratively register modules dynamically due to deep resursive type inference
+  return app.use(chatModule).use(snippetModule);
 };
