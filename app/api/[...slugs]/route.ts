2f18c481da75a2d8a552f593ad0b9cdce47f7cea
tontan2545 <tontan2545@gmail.com>
2024-01-09 11:32:17 +0700

    feat: add PUT, PATCH, DELETE method

--- app/api/[...slugs]/route.ts
+++ app/api/[...slugs]/route.ts
@@ -2,3 +2,6 @@ import { backendApp } from "@server/index";
 
 export const GET = backendApp.handle;
 export const POST = backendApp.handle;
+export const PUT = backendApp.handle;
+export const PATCH = backendApp.handle;
+export const DELETE = backendApp.handle;
