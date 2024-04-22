1225134ed7783f165f5fd3023c28b5c679972db3
tontan2545 <tontan2545@gmail.com>
2024-04-23 01:32:19 +0700

    feat: snippet CR hooks

--- client/hooks/snippet.ts
+++ client/hooks/snippet.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { backendClient } from "../libs/api";
 
 export const useCreateSnippet = (sql: string) => {
@@ -9,6 +9,35 @@ export const useCreateSnippet = (sql: string) => {
         name,
         sql,
       });
+      if (error) throw new Error(error.name);
+      return data;
+    },
+  });
+};
+
+export const useGetAllSnippets = () => {
+  const { data, error, isLoading, isError } = useQuery({
+    queryKey: ["get-all-snippets"],
+    queryFn: async () => {
+      const { data, error } = await backendClient.api.snippet.get();
+      if (error) throw new Error(error.name);
+      return data;
+    },
+  });
+
+  return {
+    data,
+    error,
+    isLoading,
+    isError,
+  };
+};
+
+export const useGetSnippetById = (id: string) => {
+  return useQuery({
+    queryKey: ["get-snippet-by-id", id],
+    queryFn: async () => {
+      const { data, error } = await backendClient.api.snippet[id].get();
       if (error) throw new Error(error.name);
       return data;
     },
